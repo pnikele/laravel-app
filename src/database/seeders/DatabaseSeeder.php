e93afb139e9fc07b0d367b1c0d1c11dfc0b092ae
pnikele <paula.nikele@gmail.com>
2023-05-24 23:22:09 +0300

    chore: Table Factories, Seeder using Factories

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Address;
+use App\Models\Meter_reading;
+use App\Models\Reader_installation;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //Address::factory(5)->create();
+        Reader_installation::factory(3)
+            ->has(Meter_reading::factory()->count(3))
+            ->create();
     }
 }
